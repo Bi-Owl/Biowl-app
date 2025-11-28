5f2e9c66b36ee04da08e6e7d4b8e6a6b539225fe
Fardad Goodboy <fardadgoodboy@gmail.com>
2025-11-28 15:50:30 +0330

    fix: Remove welcome text from profile card

--- frontend/src/components/dashboard/ProfileCard.vue
+++ frontend/src/components/dashboard/ProfileCard.vue
@@ -5,8 +5,7 @@
     </div>
     <div class="mr-3 text-white">
       <p class="font-bold text-base">{{ userFullName }}</p>
-      <p class="text-xs">خوش آمدید</p>
-      <div v-if="walletBalance" class="flex items-center mt-1">
+      <div class="flex items-center mt-1">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
         <p class="text-sm font-semibold mr-1">{{ formattedWalletBalance }} <span class="text-xs">تومان</span></p>
       </div>
